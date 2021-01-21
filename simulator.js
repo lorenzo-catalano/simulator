63e65f6edae3528c46037ba2084ee0c89dde715a
francescoparisi <francesco.parisi@mitobit.com>
2021-01-21 10:33:55 +0100

    nuovi test

--- simulator.js
+++ simulator.js
@@ -6,10 +6,10 @@ var app = require('./app.js');
 
 var port = +(process.env.SIMULATOR_PORT || 9087) 
 
-const httpsOptions = {
+/*const httpsOptions = {
     key: fs.readFileSync('./certs/psplorenzo_1024.key'),
     cert: fs.readFileSync('./certs/psplorenzo_1024.crt')
-};
+};*/
 
 http.createServer(app).listen(port,()=>{ console.log('HTTP listening on port '+port)});
-https.createServer(httpsOptions, app).listen(port+1,()=>{ console.log('HTTPS listening on port '+(port+1))});
+//https.createServer(httpsOptions, app).listen(port+1,()=>{ console.log('HTTPS listening on port '+(port+1))});
